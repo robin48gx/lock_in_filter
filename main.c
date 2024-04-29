ed3dca6d30edd16c899fbfe4043af443ff0ef52e
Robin Clark <robin@energytechnologycontrol.com>
2024-04-29 09:38:11 +0100

    eaned up main, mainly removed commented out code

--- main.c
+++ main.c
@@ -17,13 +17,9 @@
 #define DC_TERM 2048.0
 #define BUFF_SIZE (10*4096)
 
-int64_t buffer[BUFF_SIZE];
 double dbuff[BUFF_SIZE], level, f_level, ff_level;
 
-int64_t z_0, z_1_z_2;
-int64_t p_0, p_1, p_2;
 
-#define TWO_T_32 4294967296.0
 
 
 //------------------------------------------------------------------------------
@@ -48,25 +44,8 @@ double no_dc_filter ( double x0 ) {
 	return y0;
 
 }
-/*
-double alpha2 = 0.1;
-double dc_pass_filter ( double x0 ) {
-	static double x1,x2,y0,y1,y2;
 
-	y0 = x0 - 2.0*(1.0-alpha2)*x1 // zeros z^0 z^-1 
-		+ (1.0-alpha2)*(1.0-alpha2)*x2  // zero z^2-2
-		+ 2.0 *  (1.0-(alpha2/2.0)) * y1 // poles closer (alpha2/2) 
-		- (1.0-alpha2/2.0)*(1.0-alpha/2.0) * y2;
 
-	x2 = x1;
-	x1 = x0;
-	y2 = y1;
-	y1 = y0;
-
-	return y0;
-
-}
-*/
 // part of the lock in filter is the integral, the lovck in rectification/amplification
 // only becomes meaningfull when a large number of noise samples can cancel out each other
 #define INCREMENT_BUFFER_SIZE 2000
@@ -161,62 +140,4 @@ int main()
 
     return 0;
 
-    // Z transform for a middle NOTCH
-    // H(Z) = 1/( (z-i.k)(z+ik) )
-    //
-    // where K = 1.0 -1/1000
-    // This should give a gain of approx 1 million to
-    // the desired frequency
-    //
-    // Z^0 = 1
-    // Z^1 = -2.(0.999)   : 1.998
-    // Z^2 = (0.999)^2    : 0.998001
-    //
-    //
-    //
-    double d1, d2;
-    d1 = 0.999;
-    d2 = d1 * d1;
-
-    p_1 = (int64_t) 2.0 *d1 * TWO_T_32;
-    p_2 = (int64_t) d2 *TWO_T_32;
-
-    printf(" p1 0x%016lx  \n", p_1);
-    printf(" p2 0x%016lx  \n", p_2);
-//      printf("%" PRId64 "\n", p_1);
-//      printf(" p_1 %I64d  p_2 %I64d \n", p_1,p_2 );
-//
-
-    // Apply notch filter
-    //
-    int64_t res;
-/*
-    for (int i = 0; i < BUFF_SIZE; i++) {
-	    // additions peformed in 64 bit
-        res = (buffer[i]<<32) - p_1 * z_1 + p_2 * z_2;
-        z_2 = z_1;
-        z_1 = res>>32;
-    printf(" res 0x%016llx \n", res      );
-    }
-*/
-    double rd, yd_1, yd_2;
-    double xd, xd_1, xd_2;
-    // now perform the filter in double precision
-    //
-    for (int i = 0; i < BUFF_SIZE; i++) {
-        // a double pole at nyquist (z+i)(z-i)
-        xd = dbuff[i];
-        rd = xd + ((1 - 0.001) * (1 - 0.001)) * yd_2
-            // double zero at d.c.
-            - xd_2;
-        ;
-
-        yd_2 = yd_1;
-        yd_1 = rd;
-
-        xd_2 = xd_1;
-        xd_1 = xd;
-
-        printf(" res %lf \n ", rd);
-    }
 }
